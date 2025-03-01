3f86c8bc02e3cf3dbd10506a110fadb697dbdb28
Анастасия <nastena.kiseleva.05@bk.ru>
2025-03-01 17:46:02 +0400

    new

--- knit241_template-master/src/main/java/sem_2/Task2_3/Main.java
+++ knit241_template-master/src/main/java/sem_2/Task2_3/Main.java
@@ -1,50 +1,5 @@
 package sem_2.Task2_3;
 
-// Создайте интерфейс Transport с методом getSpecifications(), который возвращает характеристики транспортного средства.
-interface Transport {
-    String getSpecifications();
-}
-
-// Создайте классы BusinessCar, FamilyVan и Motorbike, реализующие Transport.
-class BusinessCar implements Transport {
-    @Override
-    public String getSpecifications() {
-        return "Комфортный салон, кожаные сиденья";
-    }
-}
-
-class FamilyVan implements Transport {
-    @Override
-    public String getSpecifications() {
-        return "Поддерживает перевозку 6+ пассажиров";
-    }
-}
-
-class Motorbike implements Transport {
-    @Override
-    public String getSpecifications() {
-        return "Предназначен для быстрой доставки";
-    }
-}
-
-// Создайте класс TransportFactory с методом createTransport(), который:
-// принимает параметр типа поездки и возвращает соответствующий объект.
-// Добавьте обработку исключений, если передан неизвестный тип транспорта.
-class TransportFactory {
-    public static Transport createTransport(String tripType) throws IllegalArgumentException {
-        switch (tripType) {
-            case "business":
-                return new BusinessCar();
-            case "family":
-                return new FamilyVan();
-            case "delivery":
-                return new Motorbike();
-            default:
-                throw new IllegalArgumentException("Неизвестный тип транспорта");
-        }
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
         try {
